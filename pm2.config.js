56cdef8ff999b296457d757bcc13a47a91d68c97
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-06-07 15:49:45 +0200

    pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -3,6 +3,7 @@ export default {
     {
       name: 'mill socket',
       script: 'dist/index.js',
+      cwd: '/var/www/mill-game-socket-server/',
       interpreter: 'bun',
       env: {
         PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
