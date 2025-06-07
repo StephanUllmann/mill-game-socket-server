411fb335cecf9e41f1929d40637408de1932ae52
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-06-07 15:36:56 +0200

    pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  name: 'mill socket',
-  script: 'dist/index.js',
-  interpreter: 'bun',
-  env: {
-    PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
-  },
+export default {
+  apps: [
+    {
+      name: 'mill socket',
+      script: 'dist/index.js',
+      interpreter: 'bun',
+      env: {
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+      },
+    },
+  ],
 };
